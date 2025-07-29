d61dcf1b7cde8bc7a3f2555a02421d70cf15dbb9
Victor Ochoa <victor.silva@luizalabs.com>
2025-07-29 17:32:40 -0300

    refatoração de codigo

---
+++ Model/TestimonialModel.cs
@@ -0,0 +1,9 @@
+// Modelo para depoimento
+namespace OMGqueCheiro.Model
+{
+    public class TestimonialModel
+    {
+        public string Text { get; set; }
+        public string Author { get; set; }
+    }
+}
