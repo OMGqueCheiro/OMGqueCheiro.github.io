284b8625bbd35d048555691467bc999c47f3c4fc
Victor Ochoa <victor.silva@luizalabs.com>
2025-07-29 12:17:48 -0300

    fix criação de componentes

---
+++ Model/ProductModel.cs
@@ -0,0 +1,10 @@
+// Modelo para produto
+namespace OMGqueCheiro.Model
+{
+    public class ProductModel
+    {
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
